0f41ad7e712ec35d5d6a5045241f1e0a115c449e
jayS202 <63660559+jayS202@users.noreply.github.com>
2023-12-23 13:45:20 +0530

    Update CheckIfArrayIsSorted.cpp
    Leetcode problem on Check if Array is sorted and rotated.

--- Array/CheckIfArrayIsSorted.cpp
+++ Array/CheckIfArrayIsSorted.cpp
@@ -23,3 +23,33 @@ int main(){
     (checkArrSorted(arr, n)==true)?cout<<"Array is sorted":cout<<"Array is not sorted";
     return 0;
 }
+
+//LeetCode
+// 1752. Check if Array Is Sorted and Rotated.
+#include<bits/stdc++.h>
+using namespace std;
+
+bool checkArrSorted(vector<int> arr, int n){
+    int cnt = 0;
+    //Ex - [1,2,4,6,8]
+    //In this case the ascending order does not breaks inside the array. So cnt=0 , But when we check the last element and the first element, cnt becomes 1
+    for(int i=1;i<n;i++){
+        if(arr[i]<arr[i-1]){
+            cnt++;
+        }
+    }
+    //Ex- [5,6,9,1,2]
+    //In this case the ascending order breaks inside the array, So cnt becomes 1. But nums[n-1]<nums[0] So cnt doesn't increases. So cnt=1
+    if(arr[n-1]>arr[0]){
+        cnt++;
+    }
+    if(cnt<=1){return true;}else{return false;}
+}
+
+int main(){
+    vector<int> arr;
+    arr = {1,2,3};
+    int n = arr.size();
+    (checkArrSorted(arr, n)==true)?cout<<"Array is sorted":cout<<"Array is not sorted";
+    return 0;
+}
