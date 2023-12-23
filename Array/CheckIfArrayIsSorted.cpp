83bf0f7e3fc41335a7288faf3e76aa9031ed7542
jayS202 <63660559+jayS202@users.noreply.github.com>
2023-12-23 10:05:43 +0530

    Update CheckIfArrayIsSorted.cpp
    Code for problem Check if the is Sorted.

--- Array/CheckIfArrayIsSorted.cpp
+++ Array/CheckIfArrayIsSorted.cpp
@@ -1,1 +1,25 @@
 
+//Problem Statement: Check if the given array is sorted.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool checkArrSorted(vector<int> arr, int n){
+    for(int i=1;i<n;i++){
+        if(arr[i]>=arr[i-1]){
+
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    vector<int> arr;
+    arr = {1,3,6,7,8};
+    int n = arr.size();
+    (checkArrSorted(arr, n)==true)?cout<<"Array is sorted":cout<<"Array is not sorted";
+    return 0;
+}
