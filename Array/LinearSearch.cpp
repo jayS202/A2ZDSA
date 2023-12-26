629794075fcb20473c665797693792031876c487
jayS202 <63660559+jayS202@users.noreply.github.com>
2023-12-26 21:45:06 +0530

    Create LinearSearch.cpp
    Linear Search

---
+++ Array/LinearSearch.cpp
@@ -0,0 +1,22 @@
+// Problem Statement: Given an array, and an element num the task is to find if num is present in the given array or not. If present print the index of the element or print -1.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int search(int *arr,int n,int num){
+  for(int i=0;i<n;i++){
+    if(arr[i]==num){
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    int num = 4;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int val = search(arr,n,num);
+    printf("%d",val);
+}
