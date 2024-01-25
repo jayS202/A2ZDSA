788a04edf26af006d8a745ec21f9e648f2bf3f60
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-01-25 21:00:25 +0530

    Update TwoSum.cpp
    Two Sum

--- Array/Medium/TwoSum.cpp
+++ Array/Medium/TwoSum.cpp
@@ -76,4 +76,29 @@ int main()
 }
 
 // optimal
-//
+// TC - O(N)+O(N*log N)
+#include<bits/stdc++.h>
+using namespace std;
+
+string twoSum(int n, vector<int>& arr, int target){
+    int right = n-1;
+    int left = 0;
+    sort(arr.begin(),arr.end());
+    while(left<right){
+        int sum = arr[left] + arr[right];
+        if(sum == target){
+            return "YES";
+        }else if(sum<target)left++;
+        else right--;
+    }
+    return "NO";
+}
+
+int main(){
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    string ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant 1: " << ans << endl;
+    return 0;
+}
