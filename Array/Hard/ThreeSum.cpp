edb7c9437cd131550125cc34120d4a6f8c344873
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-03-02 13:40:37 +0530

    Update ThreeSum.cpp
    3 Sum problem

--- Array/Hard/ThreeSum.cpp
+++ Array/Hard/ThreeSum.cpp
@@ -102,7 +102,7 @@ vector<vector<int>> triplet(int n, vector<int> arr){
                 j++;
                 k--;
                 while(j<k && arr[j]==arr[j-1])j++;
-                while(j<k && arr[k]==arr[k-1])k--;
+                while(j<k && arr[k]==arr[k+1])k--;
             }
         }
     }
