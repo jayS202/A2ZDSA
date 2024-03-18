80db79805e74f4e3d62ec78099a3dd0b66c538c2
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-03-18 20:10:24 +0530

    Update BinarySearch.cpp

--- Binary Search/BS on 1D Arrays/BinarySearch.cpp
+++ Binary Search/BS on 1D Arrays/BinarySearch.cpp
@@ -1,5 +1,6 @@
 // Binary Search
 // Problem statement: You are given a sorted array of integers and a target, your task is to search for the target in the given array. Assume the given array does not contain any duplicate numbers.
+// TC - O(log N), SC - O(1)
 
 // Iterative approach
 #include<bits/stdc++.h>
