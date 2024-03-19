42fe9ef1b013e29305c15fdf9122704f4e3bc8f8
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-03-19 20:45:25 +0530

    Create SearchInsertPosition.cpp
    Search Insert Position

---
+++ Binary Search/BS on 1D Arrays/SearchInsertPosition.cpp
@@ -0,0 +1,30 @@
+// Search Insert Position
+// Problem Statement: You are given a sorted array arr of distinct values and a target value x. You need to search for the index of the target value in the array.
+
+// Lower Bound
+// TC - O(log n), SC - O(1) 
+#include<bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int>arr, int n, int x){
+    int low = 0, high = n-1;
+    int ans = n;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid]>=x){
+            ans = mid;
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 5;
+    int ind = lowerBound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}
