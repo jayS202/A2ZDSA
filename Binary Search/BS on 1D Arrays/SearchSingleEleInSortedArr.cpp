c2c25aa2f2c6567ad34ef39163bee3ca2188ee61
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-03-24 01:38:40 +0530

    Update SearchSingleEleInSortedArr.cpp
    Search Single element in a sorted array

--- Binary Search/BS on 1D Arrays/SearchSingleEleInSortedArr.cpp
+++ Binary Search/BS on 1D Arrays/SearchSingleEleInSortedArr.cpp
@@ -42,6 +42,7 @@ using namespace std;
 
 int singleNonDuplicate(vector<int> arr){
     int n = arr.size();
+    if(n==1)return arr[0];
     if(arr[0]!=arr[1])return arr[0];
     if(arr[n-1]!=arr[n-2])return arr[n-1];
     int low = 1, high = n-2;
