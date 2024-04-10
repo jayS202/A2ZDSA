c626801ccb90c3a01fe7cd0ba5f3aaf0872ba581
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-04-10 21:06:47 +0530

    Update AggressiveCows.cpp
    Aggressive Cows

--- Binary Search/BS on Answers/AggressiveCows.cpp
+++ Binary Search/BS on Answers/AggressiveCows.cpp
@@ -64,7 +64,7 @@ bool canWePlace(vector<int> &stalls, int dist, int cows){
 int aggressiveCows(vector<int> &stalls, int cows){
     sort(stalls.begin(),stalls.end());
     int n = stalls.size();
-    int low = 0, high = n-1;
+    int low = 1, high = stalls[n-1]-stalls[0];
     while(low<=high){
         int mid = (low + high)/2;
         if(canWePlace(stalls, mid, cows)==true){
