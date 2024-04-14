3139eeffc8f875284cd28b0857ae9083b19a6492
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-04-14 14:15:16 +0530

    Update SplitArrayLargestSum.cpp

--- Binary Search/BS on Answers/SplitArrayLargestSum.cpp
+++ Binary Search/BS on Answers/SplitArrayLargestSum.cpp
@@ -5,6 +5,7 @@
 // Problem Statement: Given an integer array ‘A’ of size ‘N’ and an integer ‘K'. Split the array ‘A’ into ‘K’ non-empty subarrays such that the largest sum of any subarray is minimized. Your task is to return the minimized largest sum of the split.
 // A subarray is a contiguous part of the array.
 
+// LeetCode solution
 class Solution {
 private:
     int func(vector<int> &nums, int target, int n){
