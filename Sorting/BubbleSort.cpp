ce1db57f08a4d290070f8363866bc308589b55b4
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-07-14 18:18:31 +0530

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -3,13 +3,10 @@ using namespace std;
 
 void bubblesort(vector<int> &arr, int n){
     for(int i=0;i<n;i++){
-        int flag = 0;
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
-                flag = 1;
             }
-            if(flag = 0) break;
         }
     }
     for(auto it: arr){
