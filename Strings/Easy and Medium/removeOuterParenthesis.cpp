689aa17ce03d971de63153b10867a35fb564079f
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-08-26 14:32:11 +0530

    Update removeOuterParenthesis.cpp

--- Strings/Easy and Medium/removeOuterParenthesis.cpp
+++ Strings/Easy and Medium/removeOuterParenthesis.cpp
@@ -1,7 +1,7 @@
 // Problem Statement: A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
 
 // TC - O(N) SC - O(1)
-
+// Brute
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -23,6 +23,39 @@ string removeOuterParenthesis(string str){
         }
     }
     return temp;
+}
+
+int main()
+{
+    string s = "(()())(())(()(()))";
+    string res = removeOuterParenthesis(s);
+    cout<<res<<"\n";
+    return 0;
+}
+
+// TC - O(N) SC - O(1)
+//Optimal
+#include<bits/stdc++.h>
+using namespace std;
+
+string removeOuterParenthesis(string str){
+    int cnt = 0;
+    int pos = 0;
+    int n = str.size();
+    for(int i=0;i<n;i++){
+        if(str[i]=='('){
+            cnt++;
+            if(cnt>1){
+                str[pos++] = '(';
+            }
+        }else{
+            cnt--;
+            if(cnt!=0){
+                str[pos++] = ')';
+            }
+        }
+    }
+    return str.substr(0,pos);
 }
 
 int main()
