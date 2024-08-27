d0584423aad5db35060d4489efb6494cbf1ff31d
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-08-27 10:07:00 +0530

    Create largestOddNumber.cpp
    Largest Odd Number

---
+++ Strings/Easy and Medium/largestOddNumber.cpp
@@ -0,0 +1,54 @@
+// You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+// SC - O(N) TC - O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string largestOddNum(string s){
+    int n = s.size();
+    int track = -1;
+    for(int i=n-1;i>=0;i--){
+        int number = s[i]-'0';
+        if(number%2==1){
+            track = i;
+            break;
+        }
+    }
+    if(track == -1){
+        return "";
+    }
+    return s.substr(0,track+1);
+}
+
+int main()
+{
+    string s = "35427";
+    string num = largestOddNum(s);
+    cout<<num<<"\n";
+    return 0;
+}
+
+// The above code can also be return as below:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string largestOddNum(string s){
+    int n = s.size();
+    int track = -1;
+    for(int i=n-1;i>=0;i--){
+        if((s[i]-'0')%2==1){
+            return s.substr(0,i+1);
+        }
+    }
+    return "";
+}
+
+int main()
+{
+    string s = "52";
+    string num = largestOddNum(s);
+    cout<<num<<"\n";
+    return 0;
+}
