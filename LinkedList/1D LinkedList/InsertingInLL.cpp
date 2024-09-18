46d8c85554a0ffc106d3b68a80260996e9df45b2
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-09-18 13:11:58 +0530

    Update InsertingInLL.cpp

--- LinkedList/1D LinkedList/InsertingInLL.cpp
+++ LinkedList/1D LinkedList/InsertingInLL.cpp
@@ -1,32 +1,22 @@
-// Inserting at the Head
-// Inserting at the tail
-
 #include<bits/stdc++.h>
 using namespace std;
 
 class Node{
-    public:
-        int data;
-        Node* next;
-    public:
-        Node(int data1,Node* next1){
-            data = data1;
-            next = next1;
-        }
-        Node(int data1){
-            data = data1;
-            next = nullptr;
-        }
+  public:
+    int data;
+    Node* next;
+  public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
 };
 
-void traverse(Node* head){
-    Node* temp = head;
-    while(temp){
-        cout<<temp->data<<" ";
-        temp = temp->next;
-    }
-}
-
+// Convert Array to LinkedList
 Node* convertToLL(vector<int> &arr){
     Node* head = new Node(arr[0]);
     Node* mover = head;
@@ -38,33 +28,107 @@ Node* convertToLL(vector<int> &arr){
     return head;
 }
 
-// Inserting at the head
+// Traverse
+void Traverse(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+
+// Insert At Head
 Node* insertAtHead(Node* head, int val){
     Node* head1 = new Node(val, head);
     return head1;
 }
 
-// Inserting at the tail
+// Insert At Tail
 Node* insertAtTail(Node* head, int val){
+    if(head == NULL){
+        return new Node(val);
+    }
+    
+    Node* newNode = new Node(val);
     Node* temp = head;
-    while(temp->next!=nullptr){
+    while(temp->next!=NULL){
         temp = temp->next;
     }
-    Node* tail = new Node(val);
-    temp->next = tail;
+    temp->next = newNode;
+    return head;
+}
+
+// Insert at the given position 
+Node* insertAtPos(Node* head, int val, int pos){
+    if(head == NULL){
+        if(pos==1)return new Node(val);
+    }
+    
+    if(pos == 1){
+        Node* head1 = new Node(val);
+        head1->next = head;
+        return head1;
+    }
+    Node* prev = NULL;
+    int cnt = 0;
+    Node* temp = head;
+    while(temp){
+        cnt++;
+        if(cnt == pos){
+            Node* newNode = new Node(val);
+            prev->next = newNode;
+            newNode->next = temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+// Insert or Replace a given value
+Node* insertOnValue(Node* head, int val, int onVal){
+    
+    if(head->data == onVal){
+        Node* newNode = new Node(val);
+        Node* temp = head->next;
+        delete head;
+        newNode->next = temp;
+        return newNode;
+    }
+    
+    Node* prev = NULL;
+    Node* temp = head;
+    while(temp){
+        Node* newNode = new Node(val);
+        if(temp->data == onVal){
+            prev->next = newNode;
+            newNode->next = temp->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
     return head;
 }
 
 int main()
 {
-    vector<int> arr = {3,5,2,6,1};
+    vector<int> arr = {3,5,1,4,7};
     Node* head = convertToLL(arr);
-    traverse(head);
-    cout<<"\n";
-    Node* head1 = insertAtHead(head, 9);
-    traverse(head1);
-    cout<<"\n";
-    Node* head2 = insertAtTail(head1, 10);
-    traverse(head2);
+    Traverse(head);
+    cout<<endl;
+    Node* head1 = insertAtHead(head, 6);
+    Traverse(head1);
+    cout<<endl;
+    Node* head2 = insertAtTail(head, 9);
+    Traverse(head2);
+    cout<<endl;
+    Node* head3 = insertAtPos(head, 2, 4);
+    Traverse(head3);
+    cout<<endl;
+    Node* head4 = insertOnValue(head, 8, 1);
+    Traverse(head4);
     return 0;
 }
