896b89635f8ad117f13f94da3372340a22277f80
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-10-11 14:43:50 +0530

    Update MergeTwoSortedLL.cpp

--- LinkedList/Medium problems of LL/MergeTwoSortedLL.cpp
+++ LinkedList/Medium problems of LL/MergeTwoSortedLL.cpp
@@ -114,3 +114,83 @@ int main()
     Traverse(h3);
     return 0;
 }
+
+// Optimal
+// TC - O(N) where N = N1 + N2, SC - O(1) 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    
+    Node(int data1){
+        data = data1;
+        next = NULL;
+    }
+};
+
+Node* convertArrToLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void Traverse(Node* head){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+
+
+Node* mergeTwoLL(Node* h1, Node* h2){
+    Node* dummyNode = new Node(-1);
+    Node* temp = dummyNode;
+    Node* t1 = h1;
+    Node* t2 = h2;
+    while(t1 !=NULL && t2!=NULL){
+        if(t1->data <= t2->data){
+            temp->next = t1;
+            temp=t1;
+            t1 = t1->next;
+        }else{
+            temp->next = t2;
+            temp = t2;
+            t2 = t2->next;
+        }
+    }
+    if(t1){
+        temp->next = t1;
+    }else{
+        temp->next = t2;
+    }
+    return dummyNode->next;
+}
+
+int main()
+{
+    vector<int> arr1 = {2,4,8,10};
+    vector<int> arr2 = {1,3,3,6,11,14,15,16};
+    Node* h1 = convertArrToLL(arr1);
+    Node* h2 = convertArrToLL(arr2);
+    Traverse(h1);
+    cout<<"\n";
+    Traverse(h2);
+    Node* h3 = mergeTwoLL(h1, h2);
+    cout<<"\n";
+    Traverse(h3);
+    return 0;
+}
