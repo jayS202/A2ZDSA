63e2e2e96c1b2004fa41ac63c2cc69d67ca3213f
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-10-14 15:06:47 +0530

    Update FindPairsWithGivenSum.cpp

--- LinkedList/Medium Problems of DLL/FindPairsWithGivenSum.cpp
+++ LinkedList/Medium Problems of DLL/FindPairsWithGivenSum.cpp
@@ -74,3 +74,84 @@ int main()
     }
     return 0;
 }
+
+// Optimal
+// TC - O(N) SC - O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+    public:
+        Node(int data1,Node* next1,Node* prev1){
+            data = data1;
+            next = next1;
+            prev = prev1;
+        }
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+            prev = nullptr;
+        }
+};
+
+// Convert Array to doubly Linked List
+Node* convertArrToDLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        temp->prev = mover;
+        mover = temp;
+    }
+    return head;
+}
+
+// Traverse through DLL
+void Traverse(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+
+vector<pair<int,int>> FindPairsWithGivenSum(Node* head, int target){
+        vector<pair<int, int>> res;
+        Node* left = head;
+        Node* temp = head;
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        Node* right = temp;
+        while(left->data<right->data){
+            if((left->data) + (right->data)==target){
+                res.push_back({left->data,right->data});
+                left = left->next;
+                right = right->prev;
+            }else if((left->data) + (right->data)<target){
+                left = left->next;
+            }else{
+                right = right->prev;
+            }
+        }
+        return res;
+}
+
+int main()
+{
+    vector<int> arr = {3,4,5,6,7,9,12,15,16,18,19,20,21,22,24,25,28,31,32,33,34,38,41,42,44,47,48,49};
+    Node* head = convertArrToDLL(arr);
+    Traverse(head);
+    int key = 50;
+    cout<<endl;
+    vector<pair<int,int>> res = FindPairsWithGivenSum(head, key);
+    for(auto it: res){
+        cout<<it.first<<" : "<<it.second<<"\n";
+    }
+    return 0;
+}
