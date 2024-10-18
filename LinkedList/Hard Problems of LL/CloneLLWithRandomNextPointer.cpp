6f84a53e0bba5448d9a6e060522e3410422530c3
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-10-18 18:00:41 +0530

    Update CloneLLWithRandomNextPointer.cpp

--- LinkedList/Hard Problems of LL/CloneLLWithRandomNextPointer.cpp
+++ LinkedList/Hard Problems of LL/CloneLLWithRandomNextPointer.cpp
@@ -83,3 +83,118 @@ int main() {
 
     return 0;
 }
+
+// Optimal 
+// TC - O(3N), SC - O(N)
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public: 
+        int data;
+        Node* next;
+        Node* random;
+        
+    Node(){
+        data = 0;
+        next = nullptr;
+        random = nullptr;
+    }
+        
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        random = nullptr;
+    }
+    
+    Node(int data1, Node* next1, Node* random1){
+        data = data1;
+        next = next1;
+        random = random1;
+    }
+};
+
+void printClonedLinkedList(Node *head) {
+    while (head != nullptr) {
+        cout << "Data: " << head->data;
+        if (head->random != nullptr) {
+            cout << ", Random: " << head->random->data;
+        } else {
+            cout << ", Random: nullptr";
+        }
+        cout << endl;
+         // Move to the next node
+        head = head->next;  
+    }
+}
+
+void cloneNodes(Node* head){
+    Node* temp = head;
+    Node* res = temp;
+    while(temp!=NULL){
+        Node* copyNode = new Node(temp->data);
+        res = res->next;
+        temp->next = copyNode;
+        temp = temp->next;
+        temp->next = res;
+        temp = temp->next;
+    }
+}
+
+void randomConnection(Node* head){
+    Node* temp = head;
+    Node* res = temp;
+    while(temp!=NULL){
+        res = temp->next;
+        if(temp->random){
+            res->random = temp->random->next;
+        }else{
+            res->random = nullptr;
+        }
+        res = res->next;
+        temp = temp->next->next;
+    }
+}
+
+Node* connectNext(Node* head){
+    Node* temp = head;
+    Node* dNode = new Node(-1);
+    Node* res = dNode;
+    while(temp!=NULL){
+        Node* front = temp->next->next;
+        res->next = temp->next;
+        temp->next = front;
+        res = res->next;
+        temp = front;
+    }
+    return dNode->next;
+}
+
+Node* cloneLL(Node* head){
+    cloneNodes(head);
+    randomConnection(head);
+    return connectNext(head);
+}
+
+int main(){
+    Node* head = new Node(7);
+    head->next = new Node(14);
+    head->next->next = new Node(21);
+    head->next->next->next = new Node(28);
+
+    // Assigning random pointers
+    head->random = head->next->next;
+    head->next->random = head;
+    head->next->next->random = head->next->next->next;
+    head->next->next->next->random = head->next;
+
+    cout << "Original Linked List with Random Pointers:" << endl;
+    printClonedLinkedList(head);
+    
+    cout<<"\n";
+    
+    Node* h1 = cloneLL(head);
+    printClonedLinkedList(head);
+
+    return 0;
+}
