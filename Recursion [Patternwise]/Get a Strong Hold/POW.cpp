1e33af09bd5eafdfd4c004ec7545f3b1bc5e1871
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-10-21 11:38:39 +0530

    Update POW.cpp

--- Recursion [Patternwise]/Get a Strong Hold/POW.cpp
+++ Recursion [Patternwise]/Get a Strong Hold/POW.cpp
@@ -27,3 +27,39 @@ int main()
     cout<<POW(x,n)<<"\n";
     return 0;
 }
+
+// Recursive solution
+// TC - O(N), SC - O(1)
+#include <bits/stdc++.h>
+using namespace std;
+
+double POWER(int x, long long n){
+    if(n==0){
+        return 1;
+    }
+    
+    if(n%2==1){
+        return x * POWER(x, n-1);
+    }else{
+        return POWER(x*x, n/2);
+    }
+}
+
+double POW(int x, int n){
+    long long nn = n;
+    
+    if(nn<0) nn = -1 * n;
+    
+    double res = POWER(x,nn);
+    
+    if(n<0) res = 1.0/res;
+    return res;
+}
+
+int main()
+{
+    int x = 2;
+    int n = 21;
+    cout<<POW(x,n)<<endl;
+    return 0;
+}
