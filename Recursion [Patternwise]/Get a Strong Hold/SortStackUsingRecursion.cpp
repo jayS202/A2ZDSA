1c76d66373fd687faa5a2ba335a51f87516e07fb
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-10-23 23:14:20 +0530

    Create SortStackUsingRecursion.cpp

---
+++ Recursion [Patternwise]/Get a Strong Hold/SortStackUsingRecursion.cpp
@@ -0,0 +1,55 @@
+// Problem Statement - Sort a stack using recursion.
+
+// TC - O(N^2), SC - O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortRec(stack<int> &s, int topElement){
+    if(s.empty() || topElement > s.top()){
+        s.push(topElement);
+        return;
+    }
+    
+    int temp = s.top();
+    s.pop();
+    sortRec(s, topElement);
+    
+    s.push(temp);
+}
+
+void sortAStack(stack<int> &s){
+    //Base case
+    if(s.empty()){
+        return;
+    }
+    
+    // Hypothesis
+    int topElement = s.top();
+    s.pop();
+    sortAStack(s);
+    
+    sortRec(s, topElement);
+}
+
+void printStack(stack<int> &s){
+    while(!s.empty()){
+        cout<<s.top()<<"\n";
+        s.pop();
+    }
+}
+
+int main()
+{
+    stack<int> s;
+    s.push(10);
+    s.push(5);
+    s.push(30);
+    s.push(15);
+    s.push(20);
+    
+    sortAStack(s);
+    
+    cout<<"After Reversing: "<<"\n";
+    printStack(s);
+    return 0;
+}
