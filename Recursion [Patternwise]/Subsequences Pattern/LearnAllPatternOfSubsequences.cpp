be5e0dab5ebc936907045bbb167c748ac0ba1247
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-11-04 21:22:57 +0530

    Update LearnAllPatternOfSubsequences.cpp

--- Recursion [Patternwise]/Subsequences Pattern/LearnAllPatternOfSubsequences.cpp
+++ Recursion [Patternwise]/Subsequences Pattern/LearnAllPatternOfSubsequences.cpp
@@ -41,3 +41,44 @@ int main()
 }
 
 // 2 - Find only the first occurence of the subsequence whose sum is equal to s.
+#include<bits/stdc++.h>
+using namespace std;
+
+bool implement(int ind, vector<int> &ds, int sum, int* arr, int k, int n){
+    if(ind == n){
+        if(sum == k){
+            for(auto it: ds){
+                cout<<it<<" ";
+            }
+            cout<<"\n";
+            return true;
+        }
+        return false;
+    }
+    
+    // Take
+    ds.push_back(arr[ind]);
+    sum += arr[ind];
+    if(implement(ind+1, ds, sum, arr, k, n)==true) return true;
+    
+    // Not Take
+    ds.pop_back();
+    sum -= arr[ind];
+    if(implement(ind+1, ds, sum, arr, k, n)==true) return true;
+    
+    return false;
+}
+
+void getAllSubSumK(int *arr, int k, int n){
+    vector<int> ds;
+    implement(0, ds, 0, arr, k, n);
+}
+
+int main()
+{
+    int arr[] = {1,2,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 2;
+    getAllSubSumK(arr, k, n);
+    return 0;
+}
