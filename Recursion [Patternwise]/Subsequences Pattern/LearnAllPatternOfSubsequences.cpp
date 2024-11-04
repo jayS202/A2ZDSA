e097ee4fcd74968d5b90f4f73fd98fa2297a7a5a
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-11-04 21:30:50 +0530

    Update LearnAllPatternOfSubsequences.cpp

--- Recursion [Patternwise]/Subsequences Pattern/LearnAllPatternOfSubsequences.cpp
+++ Recursion [Patternwise]/Subsequences Pattern/LearnAllPatternOfSubsequences.cpp
@@ -1,5 +1,7 @@
 // LEARN ALL THE PATTERNS OF SUBSEQUENCES
 
+// TC - O(2^N), SC - O(N)
+
 // 1 - Find the subsequence whose sum is equal to s.
 #include<bits/stdc++.h>
 using namespace std;
@@ -72,6 +74,42 @@ bool implement(int ind, vector<int> &ds, int sum, int* arr, int k, int n){
 void getAllSubSumK(int *arr, int k, int n){
     vector<int> ds;
     implement(0, ds, 0, arr, k, n);
+}
+
+int main()
+{
+    int arr[] = {1,2,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 2;
+    getAllSubSumK(arr, k, n);
+    return 0;
+}
+
+// Count the number of subsequence whose sum is equal to s
+#include<bits/stdc++.h>
+using namespace std;
+
+int implement(int ind, int sum, int* arr, int k, int n){
+    if(ind == n){
+        if(sum == k){
+            return 1;
+        }
+        return 0;
+    }
+    
+    // Take
+    sum += arr[ind];
+    int l = implement(ind+1, sum, arr, k, n);
+    
+    // Not Take
+    sum -= arr[ind];
+    int r = implement(ind+1, sum, arr, k, n);
+    
+    return l + r;
+}
+
+void getAllSubSumK(int *arr, int k, int n){
+    cout<< implement(0, 0, arr, k, n);
 }
 
 int main()
