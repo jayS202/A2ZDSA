0e0c07686c69c6936086d815a919153a854bc4c2
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-11-14 20:20:13 +0530

    Update CombinationSum2.cpp

--- Recursion [Patternwise]/Subsequences Pattern/CombinationSum2.cpp
+++ Recursion [Patternwise]/Subsequences Pattern/CombinationSum2.cpp
@@ -46,4 +46,50 @@ int main()
 }
 
 // Optimal Approach
-// TC - , SC - 
+// TC - O(2^N), SC - O(n+k)(with k being the number of unique combinations)
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+  public:
+    void findCombination(int ind, int target, vector<int> &ds, vector<int> &arr, vector<vector<int>> &ans){
+        if(target == 0){
+            ans.push_back(ds);
+            return;
+        }
+        
+        for(int i=ind;i<arr.size();i++){
+            if(i>ind && arr[i]==arr[i-1]) continue;
+            
+            if(arr[i]>target)break;
+            
+            ds.push_back(arr[i]);
+            findCombination(i+1, target-arr[i], ds, arr, ans);
+            ds.pop_back();
+        }
+    }
+  
+    vector<vector<int>> combinationSum(vector<int> &arr, int target){
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(arr.begin(),arr.end());
+        findCombination(0, target, ds, arr, ans);
+        return ans;
+    }
+};
+
+int main()
+{
+  Solution obj;
+  vector < int > v {10,1,2,7,6,1,5};
+  int target = 8;
+
+  vector < vector < int >> ans = obj.combinationSum(v, target);
+  cout << "Combinations are: " << endl;
+  for (int i = 0; i < ans.size(); i++) {
+    for (int j = 0; j < ans[i].size(); j++)
+      cout << ans[i][j] << " ";
+    cout << endl;
+  }    
+  return 0;
+}
