c07566fc4f92b865255f17727dd8d38bf78e3a96
jayS202 <63660559+jayS202@users.noreply.github.com>
2025-04-19 14:15:20 +0530

    Update secondLargestEle.cpp

--- Array/Easy/secondLargestEle.cpp
+++ Array/Easy/secondLargestEle.cpp
@@ -85,3 +85,98 @@ int main(){
     cout<<secondlargestele(arr, n)<<endl;
     return 0;
 }
+
+
+
+
+
+// Second Attempt
+#include<bits/stdc++.h>
+using namespace std;
+
+// Better
+// TC - O(N logN), SC - O(1)
+void getElements(int *arr, int n){
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+    int largest = arr[n-1];
+    cout<<"Largest: "<<largest<<"\n";
+    int i=n-2;
+    while(arr[i]==largest){
+        i--;
+    }
+    
+    if(i>=0){
+        cout<<"Second Largest: "<<arr[i]<<"\n";
+    }else{
+        cout<<"Second Largest: Not found (all elements are equal)\n";
+    }
+    
+    int smallest = arr[0];
+    cout<<"Smallest: "<<smallest<<"\n";
+    int j=1;
+    while(arr[j]==smallest){
+        j++;
+    }
+    
+    if(j<n){
+        cout<<"Second Smallest: "<<arr[j];
+    }else{
+        cout<<"Second Smallest: Not found (all elements are equal)\n";
+    }
+}
+
+int main(){
+    int arr[]={1,4,7,7,5,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    getElements(arr,n);
+    return 0;
+}
+
+// Optimal
+#include<bits/stdc++.h>
+using namespace std;
+
+// TC - O(1), SC - O(1)
+void getElements(int *arr, int n){
+    int maxi = arr[0];
+    int secMax = INT_MIN;
+    for(int i=1;i<n;i++){
+        if(arr[i]>maxi){
+            secMax = maxi;
+            maxi = arr[i];
+        }
+        else if(arr[i]>secMax && arr[i]!=maxi){
+            secMax = arr[i];
+        }
+        cout<<"i: "<<i<<" - "<<arr[i]<<" "<<"maxi: "<<maxi<<" "<<"Sec Max: "<<secMax<<"\n";
+    }
+    
+    cout<<"Largest: "<<maxi<<"\n";
+    cout<<"Second Largest: "<<secMax<<"\n";
+    
+    int mini = arr[0];
+    int secMin = INT_MAX;
+    for(int i=1;i<n;i++){
+        if(arr[i]<mini){
+            secMin = mini;
+            mini = arr[i];
+        }else if(arr[i]<secMin && arr[i]!=mini){
+            secMin = arr[i];
+        }
+        cout<<"i: "<<i<<" - "<<arr[i]<<" "<<"mini: "<<mini<<" "<<"Sec Min: "<<secMin<<"\n";
+    }
+    
+    cout<<"Smallest: "<<mini<<"\n";
+    cout<<"Second smallest: "<<secMin<<"\n";
+}
+
+int main(){
+    int arr[]={1,4,7,7,5,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    getElements(arr,n);
+    return 0;
+}
