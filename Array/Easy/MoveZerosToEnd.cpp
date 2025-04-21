11299a5c903ee06edce50775ebc9b89c9d1ac728
jayS202 <63660559+jayS202@users.noreply.github.com>
2025-04-21 11:50:15 +0530

    Update MoveZerosToEnd.cpp

--- Array/Easy/MoveZerosToEnd.cpp
+++ Array/Easy/MoveZerosToEnd.cpp
@@ -33,25 +33,66 @@ int main(){
     return 0;
 }
 
-
-//Optimal: In-place
+// Better
+// TC - O(N), SC - O(N)
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> moveZeros(int n, vector<int> arr){
-  int k=0;
-  for(int i=0;i<n;i++){
-    if(arr[i]!=0){
-      arr[k++]=arr[i];
+vector<int> moveZeros(int n, vector<int> &arr){
+    int i=0;
+    int j=n-1;
+    vector<int> ans(n);
+    for(int k=0;k<n;k++){
+        if(arr[k]==0){
+            ans[j] = arr[k];
+            j--;
+        }else{
+            ans[i] = arr[k];
+            i++;
+        }
     }
-  }
-  for(int i=k;i<n;i++){
-    arr[i]=0;
-  }
-  return arr;
+    return ans;
 }
 
-int main(){
+int main()
+{
+    vector<int> arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+    int n = 10;
+    vector<int> ans = moveZeros(n, arr);
+    for (auto &it : ans) {
+        cout << it << " ";
+    }
+    cout << '\n';
+    return 0;
+}
+
+//Optimal: In-place
+// TC - O(N), SC - O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> moveZeros(int n, vector<int> &arr){
+    int j = -1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            j = i;
+            break;
+        }
+    }
+    
+    if(j==-1)return arr;
+    
+    for(int i=j+1;i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+    return arr;
+}
+
+int main()
+{
     vector<int> arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
     int n = 10;
     vector<int> ans = moveZeros(n, arr);
