16ee6b3430d874eb45300f200e4e2e5e0d19f684
jayS202 <63660559+jayS202@users.noreply.github.com>
2025-10-06 17:16:55 +0530

    Update SortArrByRecursion.cpp

--- Recursion [Patternwise]/Get a Strong Hold/SortArrByRecursion.cpp
+++ Recursion [Patternwise]/Get a Strong Hold/SortArrByRecursion.cpp
@@ -52,3 +52,55 @@ int main()
     printArray(arr, n);
     return 0;
 }
+
+// Sorting Array using Recursion
+// We'll use a simple logic:
+// 1. Remove the last element.
+// 2. Recursively sort the remining array.
+// 3. Insert the last element into its correct position in the sorted Array.
+
+// Approach:
+// 1. sortArray(vector<int> &arr)
+// - Sorts the array recursively.
+// - Base case: If only one element -> already sorted
+
+// 2. insert(vector<int> &arr, int temp)
+// - Insert an element temp into the sorted array arr.
+
+// Code:
+#include<bits/stdc++.h>
+using namespace std;
+
+void insert(vector<int> &arr, int temp){
+    if(arr.size()==0 || arr.back()<=temp){
+        arr.push_back(temp);
+        return;
+    }
+    
+    int val = arr.back();
+    arr.pop_back();
+    
+    insert(arr, temp);
+    
+    arr.push_back(val);
+}
+
+void sortArray(vector<int> &arr){
+    if(arr.size()==1)return;
+    
+    int temp = arr.back();
+    arr.pop_back();
+    
+    sortArray(arr);
+    
+    insert(arr, temp);
+}
+
+int main(){
+    vector<int> arr = {3, 4, 2, 5, 1, 6};
+    sortArray(arr);
+    for(auto it: arr){
+        cout<<it<<" ";
+    }
+    return 0;
+}
