55774554d0a0720d1010fb3a7794d4b9efac3db4
jayS202 <63660559+jayS202@users.noreply.github.com>
2025-10-07 12:37:26 +0530

    Update AllPossibleSubsequences.cpp

--- Recursion [Patternwise]/Subsequences Pattern/AllPossibleSubsequences.cpp
+++ Recursion [Patternwise]/Subsequences Pattern/AllPossibleSubsequences.cpp
@@ -1,5 +1,43 @@
 // Generates all subsequences (i.e., all combinations of elements preserving order but not necessarily contiguous).
 // TC - O(2^N), SC - O(N)
+
+// 													ind = 0
+// 													arr=[3, 2, 1]
+// 														 0, 1, 2
+														 
+														 
+														 
+// 													f(arr, 0, [])
+// 											take		   |           Not take
+// 							-----------------------------------------------------------------------------
+// 							|                       													|
+// 						f(arr, 1, [3])    													f(arr, 1, [])
+// 					take    |	 Not take												   take    |    Not take
+// 				---------------------------  											---------------------------------
+// 			    |                         |                                             |                                |
+// 			f(arr, 2, [3,2])          f(arr, 2, [3])                               f(arr, 2, [2])                   f(arr, 2, []) 
+// 			    |                              |                                        |                                    |
+// 		-------------------                    -------------------                --------------------                 ------------------
+// 	    |                  |                   |                 |                |                  |                |                 |
+// 	f(arr, 3, [3,2,1]) f(arr, 3, [3,2])      f(arr, 3, [3,2]) f(arr, 3, [3])    f(arr, 3, [2,1]) f(arr, 3, [2])     f(arr, 3, [1]) f(arr, 3, [])
+	
+
+// func(arr, ind, ds){
+// 	if(ind>=arr.size()){
+// 		print(ds);
+// 		return;
+// 	}
+	
+// 	// Take
+// 	ds.add(arr[ind]);
+// 	func(arr, ind+1, ds); 
+	
+// 	// Not Take
+// 	ds.remove(arr[ind]);
+// 	func(arr, ind+1, ds);
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
