df0e1062bf4865e6640771e2ba96a3e133ed86a8
jayS202 <63660559+jayS202@users.noreply.github.com>
2025-10-25 15:26:21 +0530

    Update SieveOfEratosthenes.cpp

--- Bit Manipulation/Advanced Maths/SieveOfEratosthenes.cpp
+++ Bit Manipulation/Advanced Maths/SieveOfEratosthenes.cpp
@@ -22,3 +22,40 @@ int main()
     }
     return 0;
 }
+
+// For given multiple ranges
+#include<bits/stdc++.h>
+using namespace std;
+
+void sieveOfErothstatus(vector<pair<int,int>> &arr){
+    for(auto it: arr){
+        int query = max(it.first, it.second);
+        vector<int> ans(query+1, 1);
+        vector<int> ls;
+        
+        ans[0] = ans[1] = 0;
+        for(int i=2;i*i<=query;i++){
+            if(ans[i]==1){
+                for(int j=i*i;j<=query;j+=i){
+                    if(ans[j]!=0) ans[j] = 0;
+                }
+            }
+        }
+        
+        for(int k=it.first;k<=it.second;k++){
+            if(ans[k] == 1) ls.push_back(k);
+        }
+        
+        for(auto it: ls){
+            cout<<it<<" ";
+        }
+        cout<<'\n';
+    }
+}
+
+int main(){
+    vector<pair<int,int>> arr = {{2,4},{4,7}};
+    sieveOfErothstatus(arr);
+    return 0;
+}
+    
