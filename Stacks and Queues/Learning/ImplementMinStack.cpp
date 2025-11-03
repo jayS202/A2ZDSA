25faf4f69ec4164b13c4e7f7d66705f2cada38f2
jayS202 <63660559+jayS202@users.noreply.github.com>
2025-11-03 18:43:43 +0530

    Fix complexity comments in ImplementMinStack.cpp
    Updated time and space complexity comments for clarity.

--- Stacks and Queues/Learning/ImplementMinStack.cpp
+++ Stacks and Queues/Learning/ImplementMinStack.cpp
@@ -2,7 +2,7 @@
 
 // Approach 1:
 // Each time we push a new element, we compare it with the current minimum and record the smaller one alongside it. This way, the stack always remembers the minimum up to that point.
-// TC - O(1), SC - O(N)
+// TC - O(1), SC - O(2N)
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -53,5 +53,86 @@ int main(){
     
     s.top();
     s.getMin();
+    return 0;
+}
+
+// Approach 2:
+// TC - O(1), SC - O(N)
+#include<bits/stdc++.h>
+using namespace std;
+
+class Stack{
+    public:
+        stack<int> st;
+        int mini = INT_MAX;
+
+        void push(int n){
+            if(st.empty()){
+                mini = n;
+                st.push(n);
+            }
+            else{
+                if(n<mini){
+                    int newVal = (2*n)-mini;
+                    st.push(newVal);
+                    mini = n;
+                }else{
+                    st.push(n);
+                }   
+            }
+        }
+        
+        void pop(){
+            if(st.empty()){
+                cout<<"Stack is empty!"<<'\n';
+                return;
+            }
+            int x = st.top();
+            st.pop();
+            if(x<mini){
+                mini = (2*mini)-x;
+            }
+        }
+        
+        int top(){
+            if(st.empty()){
+                cout<<"Stack is empty"<<'\n';
+                return -1;
+            }
+            int el = st.top();
+            if(el<mini){
+                return mini;
+            }else{
+                return el;
+            }
+        }
+        
+        int getMin(){
+            if (st.empty()) {
+                cout << "Stack is empty!" << '\n';
+                return -1; // sentinel
+            }
+
+            return mini;
+        }
+};
+
+int main(){
+    Stack s;
+    
+    cout << "Current Min: " << s.getMin() << '\n';
+    
+    s.push(-2);
+    s.push(0);
+    s.push(-3);
+    // s.push(2);
+
+    cout << "Current Min: " << s.getMin() << '\n';
+    cout << "Top: " << s.top() << '\n';           // 2
+
+    s.pop();
+    cout << "Current Min: " << s.getMin() << '\n'; // 3
+    cout << "Top: " << s.top() << '\n';           // 7
+
     return 0;
 }
