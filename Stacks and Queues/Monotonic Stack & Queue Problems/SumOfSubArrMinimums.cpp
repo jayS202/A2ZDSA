8767274f35e3655a1b254b18a0cb0f783ac9a6e2
jayS202 <63660559+jayS202@users.noreply.github.com>
2026-01-09 14:56:07 +0530

    Update SumOfSubArrMinimums.cpp

--- Stacks and Queues/Monotonic Stack & Queue Problems/SumOfSubArrMinimums.cpp
+++ Stacks and Queues/Monotonic Stack & Queue Problems/SumOfSubArrMinimums.cpp
@@ -24,3 +24,45 @@ int main(){
     cout<<res<<"\n";
     return 0;
 }
+
+
+// Optimal Approach
+// TC - O(N), SC-O(N)
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> nse(n);
+        vector<int> pse(n);
+        stack<int> st;
+        stack<int> pst;
+        long long total = 0;
+        long long MOD = 1'000'000'007;
+        
+        // NSE
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()] > arr[i]) st.pop();
+            nse[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        // PSE
+        for(int i=0;i<n;i++){
+            while(!pst.empty() && arr[pst.top()] >= arr[i]) pst.pop();
+            pse[i] = pst.empty() ? -1 : pst.top();
+            pst.push(i); 
+        }
+
+        for(int i=0;i<n;i++){
+            long long left = i-pse[i];
+            long long right = nse[i]-i;
+            // total = (total + (left*right*arr[i]*1ll)%MOD ) % MOD;
+            long long contrib = ((left % MOD)*(right % MOD))%MOD;
+            contrib = (contrib * (arr[i]%MOD))%MOD;
+            total = (total + contrib)%MOD;
+        }
+        return (int)total; 
+    }
+};
+
+
